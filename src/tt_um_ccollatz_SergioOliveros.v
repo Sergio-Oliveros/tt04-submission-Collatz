cac480e516d3f8d1659eff936c443d51b31008da
Sergio Sebastián Oliveros Sepúlveda <67770102+Sergio-Oliveros@users.noreply.github.com>
2023-09-04 13:15:10 -0500

    Add files via upload

--- src/tt_um_ccollatz_SergioOliveros.v
+++ src/tt_um_ccollatz_SergioOliveros.v
@@ -5,8 +5,9 @@ module tt_um_ccollatz_SergioOliveros(
     input clk, //Clock
     input ena, //Start
     input rst_n, //Reset
+    input [7:0] uio_in,
     input [7:0] ui_in, //Numero de entrada - N
-    output reg [7:0] uio_out, //Contador de iteraciones - c
+    output wire [7:0] uio_out, //Contador de iteraciones - c
     output wire [7:0] uo_out, //Busy - busy
     output wire [7:0] uio_oe 
     //output reg [7:0] n
@@ -29,13 +30,14 @@ module tt_um_ccollatz_SergioOliveros(
     reg [1:0] rn;
     reg [1:0] presente = inicio;
     reg [1:0] futuro;
-    
+    reg [7:0] uio_outr;
+    assign uio_out = uio_outr;
     ////////////////Datapath 1//////////////////
     always@(posedge clk) begin
-        uio_out <= rca;               
+        uio_outr <= rca;               
     end 
    
-    assign eca = (ec)?(16'b1+uio_out):uio_out;
+    assign eca = (ec)?(16'b1+uio_outr):uio_outr;
     assign rca = (rc)?16'b0:eca;
     ////////////////Datapath 2//////////////////
     always@(posedge clk) begin
@@ -49,11 +51,11 @@ module tt_um_ccollatz_SergioOliveros(
             2'b10: n <= ((16'd3)*n)+(16'd1);
             default: n <= ui_in;
         endcase 
-           ////////////////MÃ¡quina de estados//////////////////
+           ////////////////Máquina de estados//////////////////
     ////////////////Registro de estados//////////////////
     always@(posedge clk)
         presente <= futuro;
-    ////////////////LÃ³gica del estado siguiente//////////////////
+    ////////////////Lógica del estado siguiente//////////////////
     always@(*) begin
         case(presente)
             inicio:
@@ -80,7 +82,7 @@ module tt_um_ccollatz_SergioOliveros(
                 futuro <= inicio;
         endcase
     end
-    ////////////////LÃ³gica de salida//////////////////
+    ////////////////Lógica de salida//////////////////
     always@(*)
         case(presente)  
         inicio: {ec,rc,rn[1],rn[0],busy} = 5'b01110;
